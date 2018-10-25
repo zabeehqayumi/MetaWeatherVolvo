ec460418c115c22ce7bd752163d98b8165c93c45
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-10-25 13:50:56 -0700

    Initial

--- MetaWeatherVolvo/ViewController.swift
+++ MetaWeatherVolvo/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        jsonFileLoGothenburg()
+        jsonFileLooStockholm()
+ 
+    }
+    
+
+    func jsonFileLoGothenburg(){
+        
+        let url = URL(string: "https://www.metaweather.com/api/location/search/?query=gothenburg")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            guard let data = data else {return}
+            
+            do{
+                let json = try JSON(data: data)
+                print(json)
+                
+            }catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
+        
+       
+        
+    }
+    
+    func jsonFileLooStockholm(){
+        
+        let url = URL(string: "https://www.metaweather.com/api/location/search/?query=stockholm")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            guard let data = data else {return}
+            
+            do{
+                let json = try JSON(data: data)
+                print(json)
+                
+            }catch{
+                print(error.localizedDescription)
+            }
+            }.resume()
+        
+        
+        
     }
 
 
